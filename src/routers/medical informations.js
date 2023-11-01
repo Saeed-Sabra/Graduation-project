a3144ad09139bac2e985dce79cb7d45b7a208215
Saeed Mohammad Sabra <saeedsabra565@gmail.com>
2023-11-01 18:15:41 +0200

    History API

--- src/routers/medical informations.js
+++ src/routers/medical informations.js
@@ -15,6 +15,26 @@ router.post("/users/info/:id", async (req, res) => {
   }
 });
 
+router.get("/users/history", async (req, res) => {
+  const token = req.header("Authorization").replace("Bearer ", "");
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    console.log(decoded.userId);
+    const info = await MedInf.find({ user: decoded.userId })
+      .populate("user", "name")
+      .sort("-createdAt");
+
+    if (!info) {
+      return res.status(400).send({
+        message: "No history found",
+      });
+    }
+
+    res.status(200).send(info);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
 
