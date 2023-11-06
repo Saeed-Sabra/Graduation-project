be0ee09ccbf5443d40b13e3e742aded9cf948ff2
AdhamWebDev <adhamabuhantash20@gmail.com>
2023-11-06 17:55:44 +0200

    CORS Fixed

--- src/app.js
+++ src/app.js
@@ -5,10 +5,17 @@ const adminRouter = require("./routers/Admin");
 const midRouter = require("./routers/medical informations");
 const authJwt = require("../middleware/jwt");
 const errorHandler = require("../middleware/error-handler");
+const cors = require('cors');
 
 const app = express();
-
 const port = process.env.PORT;
+
+
+app.use(cors({
+  origin: 'http://localhost:3001', // Replace with your React app's origin
+  methods: 'get,post',
+  optionsSuccessStatus: 204,
+}));
 
 app.use(express.json());
 app.use(authJwt());
