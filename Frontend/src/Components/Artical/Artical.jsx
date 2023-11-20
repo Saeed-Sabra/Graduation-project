486e418544a0ba479b04cda1fc948898e9397f3c
AdhamWebDev <adhamabuhantash20@gmail.com>
2023-11-20 12:34:43 +0200

    Login Backend Validation

--- Frontend/src/Components/Artical/Artical.jsx
+++ Frontend/src/Components/Artical/Artical.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
+
 export default function Artical() {
   return (
-        <div className='row m-auto shadow-lg  my-5 rounded bg-white'>
-            <div className="col-lg-6">
-                <img src="assets/blood-pressure.jpg" className='w-100' alt="blood-pressure" />
-            </div>
+    <div className='row m-auto shadow-lg my-5 rounded bg-white'>
+      <div className="col-lg-6">
+        <img src="assets/blood-pressure.jpg" className='w-100' alt="blood-pressure" />
+      </div>
 
-            <div className='col-lg-6 d-flex align-items-center'>
-                <div>
-                <span>Article</span>
-                <h2>What you need to know about blood pressure</h2>
-                <a href="">Read More</a>
-                </div>
-            </div>
+      <div className='col-lg-6 d-flex align-items-center'>
+        <div className="p-4"> {/* Added padding to the content */}
+          <span>Article</span>
+          <h2 className="mb-4">What you need to know about blood pressure</h2>
+          <a href="#" className="btn btn-primary">Read More</a>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
