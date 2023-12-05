291c45c28f2d3cb816414566c5bce80211b0da7d
AdhamWebDev <adhamabuhantash20@gmail.com>
2023-12-05 19:00:56 +0200

    Admin History Updated

--- Frontend/src/Components/AdminHistory/AdminHistory.jsx
+++ Frontend/src/Components/AdminHistory/AdminHistory.jsx
@@ -7,6 +7,7 @@ import BounceLoader from 'react-spinners/BounceLoader';
 export default function AdminHistory() {
     let {id} = useParams();
     const token = localStorage.getItem('UserToken');
+    const [selectedHistory, setselectedHistory] = useState({});
 
     const [history, setHistory] = useState([]);
 
@@ -27,10 +28,12 @@ export default function AdminHistory() {
       },2000)
     }, []);
   
-  
+    const handleViewDetails = (index) => {
+      setselectedHistory(history[index]);
+    };
+    
     return (
       <>
-
 <div>
       {loading ? (
         <div className={'loaderContainer'}>
@@ -55,94 +58,91 @@ export default function AdminHistory() {
               </Toolbar>
             </AppBar>
 
-
-
  <h2 style={{ marginTop: '150px', marginBottom: '20px' }}>
   {history[0]?.user?.name}'s History
 </h2>
 
-    <div className="modal fade"  id="myModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="staticBackdropLabel">Result</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-          </div>
-          <div className="modal-body">
-            {history.map( (result)=>
-                  <div className='container text-center'>
-                    <div className="row">
-                      <div className="col-lg-6">
-                      <h5>Age:</h5> 
-                    {result.Age} 
-                      </div>
-                      <div className="col-lg-6">
-                      <h5>Gender:</h5>
-                  {result.Gender===2? <p>Male</p>
-                    : <p>Female</p>}
-                        </div>
-                  <div className="col-lg-6 mt-3">
-                    <h5>Hieght:</h5>
-                    {result.Height}
-                  </div>
-  
-                  <div className="col-lg-6 mt-3">
-                  <h5>Weight:</h5>
-                    {result.Weight}
-                  </div>
-  
-                  <div className="col-lg-6 mt-3">
-                      <h5>High Blood Pressure:</h5>
-                      {result.HighBP}
-                  </div>
-  
-                  <div className="col-lg-6 mt-3">
-                  <h5>Low Blood Pressure:</h5>
-                      {result.LowBP}
-                  </div>
-  
-                  <div className="col-lg-6 mt-3">
-                    <h5>Cholesterol:</h5>
-                    {result.Cholesterol===0? <p>Don't Know</p>:""}
-                    {result.Cholesterol===1? <p>Normal</p>:""}
-                    {result.Cholesterol===1? <p>Above Normal</p>:""}
-                    {result.Cholesterol===1? <p>Well Above Normal</p>:""}
-                  </div>
-  
-                  <div className="col-lg-6 mt-3">
-                  <h5>Glucose:</h5>
-                    {result.Glucose===0? <p>Don't Know</p>:""}
-                    {result.Glucose===1? <p>Normal</p>:""}
-                    {result.Glucose===1? <p>Above Normal</p>:""}
-                    {result.Glucose===1? <p>Well Above Normal</p>:""}
-                  </div>
-  
-            <div className="col-lg-6 mt-3">
-                <h5>Smoking:</h5>
-                {result.Smoking===0? <p>No</p>:""}
-                {result.Smoking===1? <p>Yes</p>:""}
-            </div>
-  
-            <div className="col-lg-6 mt-3">
-            <h5>Alchohol:</h5>
-                {result.Alcohol===0? <p>No</p>:""}
-                {result.Alcohol===1? <p>Yes</p>:""}
-            </div>
-  
-            <div className="col-lg-6 mt-3">
-            <h5>Activities:</h5>
-                {result.Activity===0? <p>No</p>:""}
-                {result.Activity===1? <p>Yes</p>:""}
-            </div>
-  
-            <div className="col-lg-6 mt-3">
-              <h5 className='text-danger'>Result:</h5>
-              {result.Result}
-            </div>
+<div className="modal fade mt-5" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header me-auto">
+          <h1 className="modal-title fs-5 me-auto" id="staticBackdropLabel">Result</h1>
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+        </div>
+        <div className="modal-body">
+          
+                <div className='container text-center'>
+                  <div className="row">
+                    <div className="col-lg-6">
+                    <h5>Age</h5> 
+                  {selectedHistory.Age} 
                     </div>
+                    <div className="col-lg-6">
+                    <h5>Gender</h5>
+                {selectedHistory.Gender===2? <p>Male</p>
+                  : <p>Male</p>}
+                      </div>
+                <div className="col-lg-6 mt-3">
+                  <h5>Height</h5>
+                  {selectedHistory.Height}
+                </div>
+
+                <div className="col-lg-6 mt-3">
+                <h5>Weight</h5>
+                  {selectedHistory.Weight}
+                </div>
+
+                <div className="col-lg-6 mt-3">
+                    <h5>High Blood Pressure</h5>
+                    {selectedHistory.HighBP}
+                </div>
+
+                <div className="col-lg-6 mt-3">
+                <h5>Low Blood Pressure</h5>
+                    {selectedHistory.LowBP}
+                </div>
+
+                <div className="col-lg-6 mt-3">
+                  <h5>Cholesterol</h5>
+                  {selectedHistory.Cholesterol===0? <p>DontKnow</p>:""}
+                  {selectedHistory.Cholesterol===1? <p>Normal</p>:""}
+                  {selectedHistory.Cholesterol===2? <p>AboveNormal</p>:""}
+                  {selectedHistory.Cholesterol===3? <p>WellAboveNormal</p>:""}
+                </div>
+
+                <div className="col-lg-6 mt-3">
+                <h5>Glucose</h5>
+                  {selectedHistory.Glucose===0? <p>DontKnow</p>:""}
+                  {selectedHistory.Glucose===1? <p>Normal</p>:""}
+                  {selectedHistory.Glucose===2? <p>AboveNormal</p>:""}
+                  {selectedHistory.Glucose===3? <p>WellAboveNormal</p>:""}
+                </div>
+
+          <div className="col-lg-6 mt-3">
+              <h5>Smoking</h5>
+              {selectedHistory.Smoking===0? <p>No</p>:""}
+              {selectedHistory.Smoking===1? <p>Yes</p>:""}
+          </div>
+
+          <div className="col-lg-6 mt-3">
+          <h5>Alcohol</h5>
+              {selectedHistory.Alcohol===0? <p>No</p>:""}
+              {selectedHistory.Alcohol===1? <p>Yes</p>:""}
+          </div>
+
+          <div className="col-lg-6 mt-3">
+          <h5>Activity</h5>
+              {selectedHistory.Activity===0? <p>No</p>:""}
+              {selectedHistory.Activity===1? <p>Yes</p>:""}
+          </div>
+
+          <div className="col-lg-6 mt-3">
+            <h5 className='text-danger'>Result</h5>
+            {selectedHistory.Result}
+          </div>
                   </div>
-              
-             )}
+                </div>
+
           </div>
           <div className="modal-footer m-auto">
             <button type="button" className="btn btn-primary" data-bs-dismiss="modal">Close</button>
@@ -161,26 +161,40 @@ export default function AdminHistory() {
           <tr>
             <th scope="col">#</th>
             <th scope="col">Date</th>
+            <th scope="col">Time</th>
             <th scope="col">Result</th>
             <th scope="col">Check Details</th>
           </tr>
         </thead>
         <tbody>
-          {history.map((result, index) => {
-            const createdAt = new Date(result.createdAt).toISOString();
-            const datePart = createdAt.split('T')[0];
+        {history.map((result, index) => {
+  const createdAt = new Date(result.createdAt).toISOString();
+  const datePart = createdAt.split('T')[0];
+  let timePart = createdAt.split('T')[1];
+
+  timePart = timePart.slice(0, -5);
   
             return (
               <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{datePart}</td>
-                <td>{result.Result}</td>
-                <td><button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">View Details</button></td>
+      <td>{index + 1}</td>
+      <td>{datePart}</td>
+      <td>{timePart}</td>
+      <td>{result.Result}</td>
+      <td> <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#staticBackdrop"
+          onClick={() => handleViewDetails(index)}
+        >
+          View Details
+        </button></td>
                 
 
               </tr>
             );
           })}
+
         </tbody>
       </table>
           </CardContent>
